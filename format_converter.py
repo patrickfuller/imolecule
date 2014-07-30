c41cb3af57f04310dd6c5c5048d5c373fb7b9c6f
Patrick Fuller <patrickfuller@gmail.com>
2014-07-30 09:15:25 -0500

    Format converter now Python3 compatible.

--- format_converter.py
+++ format_converter.py
@@ -1,16 +1,19 @@
 """
 Methods to interconvert between json and other (cif, mol, smi, etc.) files
 """
+from collections import Counter
+from fractions import gcd
+from functools import reduce
 
 import pybel
 ob = pybel.ob
 
-import json_formatter as json
+import mofgen.json_formatter as json
 
 table = ob.OBElementTable()
 
 
-def convert(data, in_format, out_format, filename=None, pretty=False):
+def convert(data, in_format, out_format, name=None, pretty=False):
     """Converts between two inputted chemical formats.
 
     Args:
@@ -20,20 +23,18 @@ def convert(data, in_format, out_format, filename=None, pretty=False):
             recognized by Open Babel
         out_format: The format to convert to. Can be "json" or any format
             recognized by Open Babel
-        filename: (Optional) The name of the file containing `data`. This is
-            used primarily to encode data saved in the file naming scheme of
-            the old building-block format
+        name: (Optional) If `out_format` is "json", will save the specified
+            value in a "name" property
         pretty: (Optional) If True and `out_format` is "json", will pretty-
             print the output for human readability
     Returns:
         A string representing the inputted `data` in the specified `out_format`
     """
-
     # Decide on a json formatter depending on desired prettiness
     dumps = json.dumps if pretty else json.compress
 
     # If it's a json string, load it
-    if in_format == "json" and isinstance(data, basestring):
+    if in_format == "json" and is_string(data):
         data = json.loads(data)
 
     # A little "hack" to format inputted json
@@ -41,36 +42,43 @@ def convert(data, in_format, out_format, filename=None, pretty=False):
         return json.dumps(data)
 
     # These use the open babel library to interconvert, with additions for json
-    mol = (json_to_pybel(data) if in_format == "json" else
-           pybel.readstring(in_format.encode("ascii"),
-                            "".join(i for i in data if ord(i) < 128)
-                            .encode("ascii")))
+    if in_format == "json":
+        mol = json_to_pybel(data)
+    else:
+        mol = pybel.readstring(in_format, data)
 
     # Infer structure in cases where the input format has no specification
     if not mol.OBMol.HasNonZeroCoords():
         mol.make3D()
+
+    # Make P1 if that's a thing
+    if hasattr(mol, "unitcell"):
+        mol.unitcell.FillUnitCell(mol.OBMol)
+
     mol.OBMol.Center()
 
-    # "object" does not convert to a string. "json" does.
     if out_format == "object":
-        return pybel_to_json(mol, name=filename)
+        return pybel_to_json(mol, name)
     elif out_format == "json":
-        return dumps(pybel_to_json(mol, name=filename))
+        return dumps(pybel_to_json(mol, name))
     else:
-        return mol.write(out_format.encode("ascii"))
+        return mol.write(out_format)
 
 
-def json_to_pybel(data, center=True):
+def json_to_pybel(data, infer_bonds=False):
     """Converts python data structure to pybel.Molecule.
 
     This will infer bond data if not specified.
 
     Args:
         data: The loaded json data of a molecule, as a Python object
-        center: (Optional) Centers the coordinates of the outputted molecule
+        infer_bonds (Optional): If no bonds specified in input, infer them
     Returns:
         An instance of `pybel.Molecule`
     """
+    if "building_blocks" in data:
+        data["atoms"] = [a for bb in data["building_blocks"]
+                         for a in bb["atoms"]]
     obmol = ob.OBMol()
     obmol.BeginModify()
     for atom in data["atoms"]:
@@ -79,8 +87,9 @@ def json_to_pybel(data, center=True):
         obatom.SetVector(*atom["location"])
     # If there is no bond data, try to infer them
     if "bonds" not in data or not data["bonds"]:
-        obmol.ConnectTheDots()
-        obmol.PerceiveBondOrders()
+        if infer_bonds:
+            obmol.ConnectTheDots()
+            obmol.PerceiveBondOrders()
     # Otherwise, use the bonds in the data set
     else:
         for bond in data["bonds"]:
@@ -88,9 +97,18 @@ def json_to_pybel(data, center=True):
                 continue
             obmol.AddBond(bond["atoms"][0] + 1, bond["atoms"][1] + 1,
                           bond["order"])
+
+    # Check for unit cell data
+    # Back-compatibility with an old naming scheme
+    if "periodic_connections" in data:
+        data["unitcell"] = data["periodic_connections"]
+        del data["periodic_connections"]
+    if "unitcell" in data:
+        uc = ob.OBUnitCell()
+        uc.SetData(*(ob.vector3(*v) for v in data["unitcell"]))
+        uc.SetSpaceGroup("P1")
+        obmol.CloneData(uc)
     obmol.EndModify()
-    if center:
-        obmol.Center()
     return pybel.Molecule(obmol)
 
 
@@ -99,6 +117,7 @@ def pybel_to_json(molecule, name=None):
 
     Args:
         molecule: An instance of `pybel.Molecule`
+        name: (Optional) If specified, will save a "name" property
     Returns:
         A Python dictionary containing atom and bond data
     """
@@ -118,9 +137,40 @@ def pybel_to_json(molecule, name=None):
         uc = molecule.unitcell
         output["unitcell"] = [[v.GetX(), v.GetY(), v.GetZ()]
                               for v in uc.GetCellVectors()]
+        density = (sum(atom.atomicmass for atom in molecule.atoms) /
+                   (uc.GetCellVolume() * 0.6022))
+        output["density"] = "%.2f kg / L" % density
+
+    # Save the formula to json. Use Hill notation, just to have a standard.
+    element_count = Counter(table.GetSymbol(a.atomicnum) for a in molecule)
+    hill_count = []
+    for element in ["C", "H"]:
+        if element in element_count:
+            hill_count += [(element, element_count[element])]
+            del element_count[element]
+    hill_count += sorted(element_count.items())
+
+    # If it's a crystal, then reduce the Hill formula
+    div = (reduce(gcd, (c[1] for c in hill_count))
+           if hasattr(molecule, "unitcell") else 1)
+
+    output["formula"] = "".join(n if c / div == 1 else "%s%d" % (n, c / div)
+                                for n, c in hill_count)
+    output["molecular_weight"] = "%.1f g / mol" % (molecule.molwt / div)
+
+    # If the input has been given a name, add that
     if name:
         output["name"] = name
+
     return output
+
+
+def is_string(obj):
+    """Wraps Python2.x and 3.x ways to test if string."""
+    try:
+        return isinstance(obj, basestring)
+    except NameError:
+        return isinstance(obj, str)
 
 
 if __name__ == "__main__":
