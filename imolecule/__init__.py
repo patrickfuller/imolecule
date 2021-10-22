ae6af7f31f1b8cfd9cf176642625852bd0d69dea
Patrick Fuller <patrickfuller@gmail.com>
2021-10-22 11:12:48 -0500

    Version bump

--- imolecule/__init__.py
+++ imolecule/__init__.py
@@ -1,6 +1,6 @@
 from imolecule.notebook import draw, generate, to_json  # noqa
 
 __title__ = 'imolecule'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Patrick Fuller'
 __license__ = 'MIT'
